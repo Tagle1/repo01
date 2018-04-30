8f0e27482da8c0fd7fd1563c9edf4e2007e80129
Tagle1 <alejandro.gomeztagle@apimicroservicios.com>
2018-04-30 09:47:08 -0500

    Added bgradle build

---
+++ JavaSources/MavenDemo/AnotherApp/src/main/java/org/demo/AnotherApp.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class AnotherApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
